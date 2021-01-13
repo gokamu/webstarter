c38661a4d57e683cc11ea921988161222340d83e
marshall nyasha chikari <marshall.chikari@gmail.com>
2021-01-13 19:57:22 +0200

    created grades

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :grades
   devise_for :students, controllers: {
                           registrations: "students/registrations",
                         }
