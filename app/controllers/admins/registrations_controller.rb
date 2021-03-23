211c0ed99506780a1fa5c55194e39a61626c6df6
gitnyasha <marshall.chikari@gmail.com>
2021-03-23 13:43:43 +0200

    domain setup

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -42,12 +42,12 @@ class Admins::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :name, :password, :password_confirmation])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :username, :name, :password, :password_confirmation])
   end
 
   # The path used after sign up.
