fbf7218990dab94625b48af11f73516d2307a8e7
gitnyasha <marshall.chikari@gmail.com>
2021-04-20 22:48:48 +0200

    custom domain bug

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,22 +3,6 @@ class School < ApplicationRecord
 
   mount_uploader :logo, FileUploader
   serialize :logo, JSON
-
-  # after_save do |school|  
-  #   heroku_environments = %w(production staging)
-  #   if school.custom_domain && (heroku_environments.include? Rails.env)
-  #     added = false
-  #     heroku = Heroku::API.new(api_key: ENV['HEROKU_API_KEY'])
-  #     heroku.get_custom_domains(ENV['APP_NAME']).data[:body].each do |custom_domain|
-  #       added = true if custom_domain['custom_domain'] == school.custom_domain
-  #     end
-  
-  #     unless added
-  #       heroku.post_custom_domain(ENV['APP_NAME'], school.custom_domain)
-  #       heroku.post_custom_domain(ENV['APP_NAME'], "www.#{school.custom_domain}")
-  #     end
-  #   end
-  # end 
   
   has_many :students
   has_many :grades
